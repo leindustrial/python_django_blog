0b97b9f5e0cb83587b4d5f5181c7418694f12434
leindustrial <hey@leindustrial.ru>
2024-04-21 12:02:40 +0200

    added an image field to posts

--- le_posts/models.py
+++ le_posts/models.py
@@ -7,8 +7,10 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     tags = models.CharField(max_length=200)
+    image = models.ImageField(blank=True, null=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+
 
     def publish(self):
         self.published_date = timezone.now()
